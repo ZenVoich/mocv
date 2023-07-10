ef829cc642c41f3923aa110fbc0bf3e38355760e
ZenVoich <zen.voich@gmail.com>
2023-07-10 19:30:07 +0400

    new build artefacts naming conventions

--- src/cli.js
+++ src/cli.js
@@ -31,9 +31,22 @@ let download = async (version, {silent} = {}) => {
 		return;
 	}
 
-	silent || console.log('Downloading...');
-	let platfrom = process.platform == 'darwin' ? 'macos' : 'linux64';
-	let url = `https://github.com/dfinity/motoko/releases/download/${version}/motoko-${platfrom}-${version}.tar.gz`;
+
+	let url;
+	if (parseInt(version.replaceAll('.', '')) >= parseInt('0.9.5'.replaceAll('.', ''))) {
+		let platfrom = process.platform == 'darwin' ? 'Darwin' : 'Linux';
+		let arch = process.arch.startsWith('arm') ? 'arm64' : 'x86_64';
+		// currently only x64 binaries are available
+		arch = 'x86_64';
+		url = `https://github.com/dfinity/motoko/releases/download/${version}/motoko-${platfrom}-${arch}-${version}.tar.gz`;
+	}
+	else {
+		let platfrom = process.platform == 'darwin' ? 'macos' : 'linux64';
+		url = `https://github.com/dfinity/motoko/releases/download/${version}/motoko-${platfrom}-${version}.tar.gz`;
+	}
+
+	silent || console.log(`Downloading ${url}`);
+
 	let res = await fetch(url);
 
 	if (res.status !== 200) {
